998b8bc6033bc0ed6e8ea0b3d12b3b953a64829a
Giselle Thourey <thoureygi@gmail.com>
2024-08-27 22:27:02 -0400

    Contenido componmente Homepage

---
+++ market-place/src/components/HomePage.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Register from './Register';
+import Login from './Login';
+
+const HomePage = () => {
+  const [formType, setFormType] = useState('none'); 
+
+  const handleShowForm = (formType) => {
+    setFormType(formType);
+  };
+
+  return (
+    <div className="container mt-4">
+      <h1>Prueba de ecommerce</h1>
+      <div className="btn-group">
+        <button className="btn btn-primary" onClick={() => handleShowForm('register')}>Register</button>
+        <button className="btn btn-secondary" onClick={() => handleShowForm('login')}>Login</button>
+      </div>
+      <div className="mt-4">
+        {formType === 'register' && <Register />}
+        {formType === 'login' && <Login />}
+        {formType === 'none' && <p>Selecciona una opción</p>}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
