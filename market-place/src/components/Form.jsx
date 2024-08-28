ab280f4d1a9c6352b7abae8b1d347e4270d9b562
Giselle Thourey <thoureygi@gmail.com>
2024-08-27 22:53:27 -0400

    Primera version de estilos y modificación en Main y en Form

--- market-place/src/components/Form.jsx
+++ market-place/src/components/Form.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Form = ({ onSubmit, children }) => {
   return (
-    <form onSubmit={onSubmit} className="form">
+    <form 
+      onSubmit={onSubmit} 
+      className="p-4 border rounded shadow-sm bg-light mx-auto" 
+      style={{ maxWidth: '400px' }}
+    >
       {children}
     </form>
   );
