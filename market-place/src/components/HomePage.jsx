f986ef82814390525473839ab9d284126265f805
Giselle Thourey <thoureygi@gmail.com>
2024-08-27 22:53:27 -0400

    Primera version de estilos y modificación en Main y en Form

--- market-place/src/components/HomePage.jsx
+++ market-place/src/components/HomePage.jsx
@@ -3,7 +3,7 @@ import Register from './Register';
 import Login from './Login';
 
 const HomePage = () => {
-  const [formType, setFormType] = useState('none'); 
+  const [formType, setFormType] = useState('none');
 
   const handleShowForm = (formType) => {
     setFormType(formType);
