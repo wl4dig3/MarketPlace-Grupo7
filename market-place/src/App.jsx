44f849cf1719e333e039d36c379019a183fc37df
Giselle Thourey <thoureygi@gmail.com>
2024-08-31 02:02:42 -0400

    Agregue las fechas al archivo de productos json para poder tener las novedades

--- market-place/src/App.jsx
+++ market-place/src/App.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './views/LandingPage';
 import { AuthProvider } from './context/AuthContext';
+import { ProductsProvider } from './context/ProductsContext';
 import Productos from './views/Productos';
 
 
 const App = () => {
   return (
     <AuthProvider>
+     <ProductsProvider>
       <Routes>
         <Route exact path="/" element={<LandingPage/>} />
         <Route path="/productos" element={<Productos />} />
       </Routes>
+      </ProductsProvider> 
     </AuthProvider>
   );
 };
