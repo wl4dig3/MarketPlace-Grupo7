50972eca715458caeaab12543de8ce4c920f78d6
Giselle Thourey <thoureygi@gmail.com>
2024-08-31 01:47:25 -0400

    Creacion del context de los productos

---
+++ market-place/src/context/ProductsContext.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+export const ProductsContext = createContext();
+
+export const ProductsProvider = ({ children }) => {
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await fetch('/products.json');
+                if (!response.ok) {
+                    setError('Error en la respuesta de la red');
+                    return;
+                }
+                const data = await response.json();
+                setProducts(data);
+            } catch (error) {
+                setError('Error al cargar el JSON');
+                console.error('Error al cargar el JSON:', error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+    return (
+        <ProductsContext.Provider value={{ products, error }}>
+            {children}
+        </ProductsContext.Provider>
+    );
+};
+
