3444e36137495cb9b357338ea44a630d4115b6dc
Giselle Thourey <thoureygi@gmail.com>
2024-09-05 06:21:29 -0400

    Agregar la ruta user en el index

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -8,7 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+//Rutas
+app.use('/api', userRoutes); 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
