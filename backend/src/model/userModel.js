532b399314ce9736741e20aba8c9f1f944b9ad5d
Giselle Thourey <thoureygi@gmail.com>
2024-09-05 21:19:44 -0400

    Modificacion de modelo para encontrar user con email

--- backend/src/model/userModel.js
+++ backend/src/model/userModel.js
@@ -1,4 +1,5 @@
 import pool from '../config/db.js'; 
+import bcrypt from 'bcryptjs';
 
 export const createUser = async (userData) => {
   const { username, email, phone_number, date_of_birth, password } = userData;
@@ -10,12 +11,25 @@ export const createUser = async (userData) => {
   `;
 
   const values = [username, email, phone_number, date_of_birth, password];
-
   try {
     const result = await pool.query(query, values);
     return { success: true, data: result.rows[0] };
   } catch (error) {
-    return { success: false, message: 'Error creating user in the database' };
+    console.error('Database error:', error);
+    return { success: false, message: 'Error al crear el usuario' };
   }
 };
-
+export const findUserByEmail = async (email) => {
+    const query = `SELECT id, email, phone_number, password, picture FROM users WHERE email = $1`;
+  
+    try {
+      const result = await pool.query(query, [email]);
+      if (result.rows.length > 0) {
+        return { success: true, data: result.rows[0] };
+      }
+      return { success: false, message: 'User no encontrado' };
+    } catch (error) {
+      console.error('Database query error:', error);
+      return { success: false, message: 'Error de consulta' };
+    }
+  };
