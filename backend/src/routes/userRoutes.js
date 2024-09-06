e26f63ddd4c80f5d6126cdc532cb1941752f351f
Giselle Thourey <thoureygi@gmail.com>
2024-09-05 21:23:16 -0400

    Actualizacion authMiddleware

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { createUserController } from '../controllers/userController.js';
+import { createUserController, loginUserController } from '../controllers/userController.js';
 
 const router = express.Router();
-
+//Ruta para crear usuario
 router.post('/user', createUserController);
+//Ruta para el login del usuario
+router.post('/user', loginUserController)
+
 
 export default router;
